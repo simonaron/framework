e455579d7129920af704cfa57ee2f22c0e7bb1f6
Your Name <you@example.com>
2018-03-21 21:59:20 +0100

    Demo: 06-generate-dockerfiles image-arch instead image-role-arch (less build time)

--- demo/06-generate-dockerfiles/build.sh
+++ demo/06-generate-dockerfiles/build.sh
@@ -5,15 +5,15 @@ rsync -azP ./ pi@192.168.50.114:/home/pi/project
 # build HC image on armv7l and push to registry
 ssh pi@192.168.50.114 <<- END_SSH
   cd project
-  docker build . --build-arg ARCH=armv7l -t project_hc -f Dockerfile_HC
-  docker tag project_hc 192.168.50.100:5000/project_hc
-  docker push 192.168.50.100:5000/project_hc
+  docker build . --build-arg ARCH=armv7l -t project_armv7l -f Dockerfile
+  docker tag project_armv7l 192.168.50.100:5000/project_armv7l
+  docker push 192.168.50.100:5000/project_armv7l
 END_SSH
 
 # build MC image on x86_64 and push to registry
-docker build . --build-arg ARCH=x86_64 -t project_mc -f Dockerfile_MC
-docker tag project_mc 192.168.50.100:5000/project_mc
-docker push 192.168.50.100:5000/project_mc
+docker build . --build-arg ARCH=x86_64 -t project_x86_64 -f Dockerfile
+docker tag project_x86_64 192.168.50.100:5000/project_x86_64
+docker push 192.168.50.100:5000/project_x86_64
 
 # deploy services
-docker stack deploy --compose-file docker-compose.yml demo2
+docker stack deploy --compose-file docker-compose.yml demo
